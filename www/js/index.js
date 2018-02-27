b31b22cdbe410ef90474a7ae8ff9ef1de151bbd0
David Hagan <dave@stackableregiments.com>
2018-02-27 16:16:18 +1100

    now there's a nice d3 graph for transactions, and a grid for transactions, and a page chooser and a login

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,51 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+var app = (function(){
+	var getDb = function(){
+		if (!"openDatabase" in window){
+			return undefind;
+		}
+		var db = window.openDatabase("superMembers","0.1","backingDatabaseForTheApp",1 * 1024 * 1024);
+		db.transaction(function(tx){
+			tx.executeSql("create table if not exists users (id,username,password)",[],function(tx,_results){
+				tx.executeSql("select id from users where id = ?",[0],function(tx2,results2){
+					if (results2.rows.length < 1){
+						tx.executeSql("insert into users (id,username,password) values (?,?,?)",[0,'dave','test']);
+					}
+				});
+			});
+		});
+		return db;
+	};
+	var getUserFunc = function(){
+		var db = getDb();
+		if (db === undefined){
+			return undefined;
+		}
+		db.transaction(function(tx){
+			var result = tx.executeSql("select * from users limit 1");		
+		});	
+	};
+	var loginFunc = function(u,p,successFunc,errorFunc){
+		var db = getDb();
+		if (db === undefined){
+			errorFunc();
+		}
+		db.transaction(function(tx){
+			var findUserSelect = "select * from users where username = ?";
+			tx.executeSql(findUserSelect,[u],function(tx,results){
+				var userObj = results.rows.item(0);
+				if (userObj.username == u && userObj.password == p){
+					successFunc(userObj);
+				}
+				else {
+					errorFunc();
+				}	
+			});
+		});	
+	};
+	return {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -37,7 +81,8 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function(device) {
 			var ev = this;
-        console.log('Received Device Ready Event',device,ev);
+			var db = getDb();
+			console.log('Received Device Ready Event',device,ev,db);
     },
     onLoad: function(device) {
 			var ev = this;
@@ -50,13 +95,64 @@ var app = {
     onOnline: function(device) {
 			var ev = this;
         console.log('Received Online Event',device,ev);
-    }
-};
+    },
+		login:loginFunc,
+		getUser: getUserFunc
+	};
+})();
+
 $(function(){
-	$("#page1Button").on("click",function(e){
-		window.location="index.html";
+	var MyCurrencyField = function(config){
+		jsGrid.Field.call(this,config);
+	};
+
+	MyCurrencyField.prototype = new jsGrid.Field({
+		css: "currency-field",
+		align: "right",
+		sorter: function(cash1,cash2){
+			return cash1 - cash2;
+		},
+		itemTemplate: function(cash){
+			return "$"+_.round(cash,2);
+		}
 	});
-	$("#page2Button").on("click",function(e){
-		window.location="page2.html";
+	jsGrid.fields.currency = MyCurrencyField;
+	//add a date field to jsgrid
+	var MyDateField = function(config) {
+		jsGrid.Field.call(this, config);
+	};
+	MyDateField.prototype = new jsGrid.Field({
+		css: "date-field",            // redefine general property 'css'
+		align: "center",              // redefine general property 'align'
+		sorter: function(date1, date2) {
+				return new Date(date1) - new Date(date2);
+		},
+		itemTemplate: function(value) {
+				return new Date(value).toDateString();
+		}
 	});
+	jsGrid.fields.date = MyDateField;
 });
+
+var QueryParams = (function(){
+	var search = window.location.search;
+	if (_.startsWith(search,"?")){
+		search = search.substring(1);
+	}
+	var parts = {};
+	_.forEach(search.split("&"),function(p){
+		var items = p.split('=');
+		var key = _.head(items);
+		var value = _.tail(items).join("=");
+		console.log("item",p,items,key,value);
+		parts[key] = value;
+	});
+
+	console.log("qp",search,parts);
+	var getFunc = function(key){
+		return parts[key];
+	};	
+	return {
+		get:getFunc
+	};
+})();
